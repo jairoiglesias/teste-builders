ca0f0b3037cf3d7c554f87e97b6f25b34edfc860
JAIRO WILLIAM MEIRELES IGLESIAS <jairohighwind@hotmail.com>
2018-10-23 03:46:46 -0300

    Refactoring Tests

--- src/main/java/br/com/builders/treinamento/resources/CustomerResource.java
+++ src/main/java/br/com/builders/treinamento/resources/CustomerResource.java
@@ -1,9 +1,5 @@
 package br.com.builders.treinamento.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.naming.ServiceUnavailableException;
 import javax.validation.Valid;
 import javax.ws.rs.core.MediaType;
 
@@ -12,20 +8,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.builders.treinamento.dto.request.CustomerRequest;
-import br.com.builders.treinamento.dto.response.SampleResponse;
 import br.com.builders.treinamento.repository.CustomerRepository;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping("/api")
@@ -36,31 +26,14 @@ public class CustomerResource {
 	@Autowired
 	CustomerRepository customerRepository;
 	
-	 @ApiOperation(value = "Listar Samples", httpMethod = "GET", response = CustomerResource.class)
-	  @ApiResponses(value = {@ApiResponse(code = 200, message = "Lista de Sample"),
-	      @ApiResponse(code = 500, message = "Erro interno do sistema"),
-	      @ApiResponse(code = 503, message = "Erro ao acessar um servico/servidor"),
-	      @ApiResponse(code = 400, message = "Erro nos parametros no request"),
-	      @ApiResponse(code = 422, message = "Erro de negocio")})
-	  @RequestMapping(value = "/samples", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON)
-	  @ResponseStatus(value = HttpStatus.OK)
-	  public List<SampleResponse> listarCustomers(
-	      @ApiParam(name = "apikey", required = true, value = "apikey") @RequestHeader(required = true) final String apikey)
-	      throws ServiceUnavailableException {
-	    
-	    List<SampleResponse> response = new ArrayList<>();
-//	    response.add(SampleResponse.builder().id("sample-id").build());
-
-	    return response;
-	  }
 	  																																							
-	  @RequestMapping(value = "/customers", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON)
-	  @ResponseStatus(value = HttpStatus.OK)
-	  public ResponseEntity<Object> createCustomer(@Valid @RequestBody CustomerRequest request) throws Exception{
+	@RequestMapping(value = "/customers", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON)
+	@ResponseStatus(value = HttpStatus.OK)
+	public ResponseEntity<Object> createCustomer(@Valid @RequestBody CustomerRequest request) throws Exception{
 		  
-		  return ResponseEntity.status(HttpStatus.CREATED).body(customerRepository.insert(request));
+		return ResponseEntity.status(HttpStatus.CREATED).body(customerRepository.insert(request));
 		  																																															
-	  }																																																																																																																																																																																																																																																							
+	 }																																																																																																																																																																																																																																																							
 	  
 	  @RequestMapping(value = "/customers/{customerId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON)
 	  @ResponseStatus(value = HttpStatus.OK)
@@ -70,11 +43,11 @@ public class CustomerResource {
 		  
 	  }
 	  
-	  @RequestMapping(value = "/customers/{customerId}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON)
+	  @RequestMapping(value = "/customers/{customerId}", method = RequestMethod.PATCH, produces = MediaType.APPLICATION_JSON)
 	  @ResponseStatus(value = HttpStatus.OK)
 	  public ResponseEntity<Object> createCustomer(@PathVariable("customerId") String customerId, @Valid @RequestBody CustomerRequest request) throws Exception{
 		  
-		  return ResponseEntity.status(HttpStatus.CREATED).body(customerRepository.save(request));
+		  return ResponseEntity.status(HttpStatus.OK).body(customerRepository.save(request));
 		  
 	  }
 	  
@@ -85,10 +58,5 @@ public class CustomerResource {
 		  return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	  }
 	  
-	  @RequestMapping(value = "/testev2", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON)
-	  @ResponseStatus(value = HttpStatus.OK)
-	  public String teste() {
-		  return "teste";
-	  }
 
 }
--- src/test/java/br/com/builders/treinamento/CustomerTest.java
+++ src/test/java/br/com/builders/treinamento/CustomerTest.java
@@ -1,22 +1,216 @@
 package br.com.builders.treinamento;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import net.minidev.json.JSONObject;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CustomerTest {
-
-	@Test
-	public void shouldInsertCustomer() throws Exception{
-		
-		
 	
+	private MockMvc mockMvc;
+	
+	@Autowired
+    private WebApplicationContext wac;
+	
+	String idInsert = "553fa88c-4511-445c-b33a-ddff58d76998";
+	String idUpdate = "553fa88c-4511-445c-b33a-ddff58d76886";
+	
+	@Before
+    public void setup() throws Exception {
+				
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+		
+		mockMvc.perform(MockMvcRequestBuilders.delete("/api/customers/{customerId}", idInsert))
+		.andExpect(status().isNoContent())
+		.andDo(print());
+	}
+	
+	@Test
+	public void testShouldInsertCustomer() throws Exception{
+		
+		String crmId = "C645235";
+		String baseUrl = "http://www.platformbuilders.com.br";
+		String name = "Platform Builders Test";
+		String login = "contato@platformbuilders.com.br";
+		
+		JSONObject jsonObj = new JSONObject();
+				
+		jsonObj.put("id", this.idInsert);
+		jsonObj.put("crmId", crmId);
+		jsonObj.put("baseUrl", baseUrl);
+		jsonObj.put("name", name);
+		jsonObj.put("login", login);
+		
+		mockMvc.perform(
+        MockMvcRequestBuilders.post("/api/customers")
+                .content(jsonObj.toString())
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isCreated())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+        .andDo(print());
 		
 	}
+	
+	@Test
+	public void testShouldGetCustomer() throws Exception{
+		
+		mockMvc.perform(
+        MockMvcRequestBuilders.get("/api/customers/{customerId}", this.idUpdate))
+        .andExpect(status().isOk())
+        .andDo(print());
+		
+	}
+	
+	@Test 
+	public void testShouldUpdateCustomer() throws Exception{
+		
+		String crmId = "C645235";
+		String baseUrl = "http://www.platformbuilders.com.br";
+		String name = "Plataform Builders é Topper!";
+		String login = "contato@platformbuilders.com.br";
+		
+		JSONObject jsonObj = new JSONObject();
+		
+		jsonObj.put("id", this.idUpdate);
+		jsonObj.put("crmId", crmId);
+		jsonObj.put("baseUrl", baseUrl);
+		jsonObj.put("name", name);
+		jsonObj.put("login", login);
+		
+		mockMvc.perform(MockMvcRequestBuilders.patch("/api/customers/{customerId}", this.idUpdate)
+		 .content(jsonObj.toString())
+         .contentType(MediaType.APPLICATION_JSON_UTF8)
+         .accept(MediaType.APPLICATION_JSON))
+		 .andExpect(status().isOk())
+		 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+		 .andDo(print());
+		
+	}
+	
+	@Test
+	public void testShouldDeleteCustomer() throws Exception{
+		
+		mockMvc.perform(MockMvcRequestBuilders.delete("/api/customers/{customerId}", this.idInsert))
+		 .andExpect(status().isNoContent())
+		 .andDo(print());
+
+	}
+	
+	@Test
+	public void testShouldNotInsertCard() throws Exception{
+		
+		String id = "553fa88c-4511-445c-b33a-ddff58d76000";
+		String crmId = "C645235";
+		String baseUrl = "http://www.platformbuilders.com.br";
+//		String name = "Platform Builders Test";
+		String login = "contato@platformbuilders.com.br";
+		
+		JSONObject jsonObj = new JSONObject();
+				
+		jsonObj.put("id", id);
+		jsonObj.put("crmId", crmId);
+		jsonObj.put("baseUrl", baseUrl);
+//		jsonObj.put("name", name);
+		jsonObj.put("login", login);
+		
+		mockMvc.perform(
+        MockMvcRequestBuilders.post("/api/customers")
+                .content(jsonObj.toString())
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON))
+        .andExpect(status().isBadRequest())
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+        .andDo(print());
+		
+	}
+	
+	@Test
+	public void testShouldNotGetCustomerIdEmpty() throws Exception {
+		
+		mockMvc.perform(
+	        MockMvcRequestBuilders.get("/api/customers/{customerId}", ""))
+	        .andExpect(status().isMethodNotAllowed())
+	        .andDo(print());
+		
+	}
+	
+	@Test
+	public void testShouldNotUpdateCustomerCustomerIdEmpty() throws Exception{
+		
+		String crmId = "C645235";
+		String baseUrl = "http://www.platformbuilders.com.br";
+		String name = "Plataform Builders é Topper!";
+		String login = "contato@platformbuilders.com.br";
+		
+		JSONObject jsonObj = new JSONObject();
+		
+		jsonObj.put("id", this.idUpdate);
+		jsonObj.put("crmId", crmId);
+		jsonObj.put("baseUrl", baseUrl);
+		jsonObj.put("name", name);
+		jsonObj.put("login", login);
+		
+		mockMvc.perform(MockMvcRequestBuilders.patch("/api/customers/{customerId}", "")
+		 .content(jsonObj.toString())
+         .contentType(MediaType.APPLICATION_JSON_UTF8)
+         .accept(MediaType.APPLICATION_JSON))
+		 .andExpect(status().isMethodNotAllowed())
+		 .andDo(print());
+		
+		
+	}
+	
+	@Test
+	public void testShouldNotUpdateCustomerCustomerInvalidJSON() throws Exception{
+		
+		String crmId = "C645235";
+		String baseUrl = "http://www.platformbuilders.com.br";
+//		String name = "Plataform Builders é Topper!";
+		String login = "contato@platformbuilders.com.br";
+		
+		JSONObject jsonObj = new JSONObject();
+		
+		jsonObj.put("id", this.idUpdate);
+		jsonObj.put("crmId", crmId);
+		jsonObj.put("baseUrl", baseUrl);
+//		jsonObj.put("name", name);
+		jsonObj.put("login", login);
+		
+		mockMvc.perform(MockMvcRequestBuilders.patch("/api/customers/{customerId}", this.idUpdate)
+		 .content(jsonObj.toString())
+         .contentType(MediaType.APPLICATION_JSON_UTF8)
+         .accept(MediaType.APPLICATION_JSON))
+		 .andExpect(status().isBadRequest())
+		 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+		 .andDo(print());
+		
+		
+	}
+	
+	@Test
+	public void testShouldNotDeleteCustomer() throws Exception{
+		
+		mockMvc.perform(MockMvcRequestBuilders.delete("/api/customers/{customerId}", ""))
+		 .andExpect(status().isMethodNotAllowed())
+		 .andDo(print());
+		
+	}
+	
 
 }
